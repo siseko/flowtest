7add82b34e349da5c1757112bff7d3ebd2360c5b
Siseko <s.nomavila@gmail.com>
2020-03-06 12:10:47 +0200

    reads file

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,11 @@
+const fs = require("fs");
+
 console.log("Testing");
 
 exports.handler = function(event, context, callback) {
-  console.log("-->", event);
+  console.log("-->", event.path.includes("Categories"));
+  const file = fs.readFileSync("./categories");
+  console.log("-->", file, file.toString());
   callback(null, {
     statusCode: 200,
     body: "Testing",
