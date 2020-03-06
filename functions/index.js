1cf9eaa41f6d9a6ce0e31104c17c70aa6abd20de
Siseko <s.nomavila@gmail.com>
2020-03-06 11:13:03 +0200

    Logging

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 exports.handler = async event => {
   const subject = event.queryStringParameters.name || "World";
+  console.log("Dee is 36");
   return {
     statusCode: 200,
     body: `Hello ${subject}!`
