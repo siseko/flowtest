ebdfd392c55016008dc167e4660fd1d9d6ce94e1
Siseko <s.nomavila@gmail.com>
2020-03-06 12:22:22 +0200

    test

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,22 @@
 const fs = require("fs");
+const path = require("path");
 
 console.log("Testing");
+
+//joining path of directory
+const directoryPath = path.join(__dirname, "");
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function(err, files) {
+  //handling error
+  if (err) {
+    return console.log("Unable to scan directory: " + err);
+  }
+  //listing all files using forEach
+  files.forEach(function(file) {
+    // Do whatever you want to do with the file
+    console.log(file);
+  });
+});
 
 exports.handler = function(event, context, callback) {
   let file;
