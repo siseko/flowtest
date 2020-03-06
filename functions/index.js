3246231992f4774c83d28fd265c4fe2b0eb74000
Siseko <s.nomavila@gmail.com>
2020-03-06 11:58:54 +0200

    CORS

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,11 @@ console.log("Testing");
 exports.handler = function(event, context, callback) {
   console.log(event.path);
   return {
-    body: "Testing"
+    body: "Testing",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept"
+    }
   };
 };
