909af5a3740b78cb7ca4a0898c1b1daab7a06f68
Siseko <s.nomavila@gmail.com>
2020-03-06 12:06:54 +0200

    testing

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,14 @@
-const fs = require("fs");
-
 console.log("Testing");
 
 exports.handler = function(event, context, callback) {
-  console.log(event.path);
-  const res = fs.readFileSync("categories.json");
-  console.log(res, res.toString());
-  return {
+  console.log("-->", event);
+  callback(null, {
+    statusCode: 200,
     body: "Testing",
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers":
         "Origin, X-Requested-With, Content-Type, Accept"
     }
-  };
+  });
 };
